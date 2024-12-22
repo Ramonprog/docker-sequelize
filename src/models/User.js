2d407a3370021ed5791ef447987e28634d83f9b4
Ramonprog <programador.ramon@gmail.com>
2024-12-22 16:50:54 -0300

    config: module + config para banco

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+import Sequelize,{ Model} from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init({
+        name:{
+          type:Sequelize.STRING,
+          allowNull: false
+        },
+        email:{
+          type:Sequelize.STRING,
+          allowNull: false,
+          unique: true
+        },
+        password:{
+          type:Sequelize.STRING,
+          allowNull: false
+        },
+        age:{
+          type: Sequelize.INTEGER,
+          allowNull: false
+        },
+    },
+    
+    {sequelize}
+  )}
+}
+
+export default User;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import routes from './routes.js'
+import User from './models/User.js';
+import Sequelize from 'sequelize';
+import config from './config/database.js'
 
 const app = express();
 app.use(express.json());
+
+const sequelize = new Sequelize(config);
+
+User.init(sequelize);
+
+sequelize.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+    app.listen(3000, () => {
+      console.log('Server started on port 3000');
+    });
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
