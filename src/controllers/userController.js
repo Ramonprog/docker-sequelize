c1fd073150567bcb208bd70b69c4b87acd717ec6
Ramonprog <programador.ramon@gmail.com>
2024-12-22 15:04:38 -0300

    chore: instalando sequelize

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,14 @@ class UserController {
     async createUser(req, res) {
       return res.status(200).json({ message: 'User created successfully' });
     }
+
+    async getAllUsers(req, res) {
+      return res.status(200).json({ message: 'All users retrieved successfully' });
+    }
+
+    async deleteUser(req, res) {
+      return res.status(200).json({ message: 'User deleted successfully' });
+    }
 }
 
 export default UserController;
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,9 @@ import UserController from './controllers/userController.js';
 const userController = new UserController();
 const router = express.Router();
 
+router.get('/user', userController.getAllUsers);
 router.post('/user', userController.createUser);
+router.delete('/user', userController.deleteUser);
 
 
 export default router;
